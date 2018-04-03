5bd14d127418d4492163d3a28c6611ce47c11bce
Tom Gillooly <thomas.gillooly@etu.univ-st-etienne.fr>
2018-04-03 09:31:25 +0200

    Start visdom on next free port - make sure server starts on right one

--- pix2pix.sh
+++ pix2pix.sh
@@ -13,6 +13,7 @@ else
 	source find_free_port.sh
 
 	OPTIONS="--display_port $DISPLAY_PORT"
+	VISDOM_OPTIONS="-p $DISPLAY_PORT"
 	echo "Display port = $DISPLAY_PORT"
 fi
 
@@ -22,9 +23,9 @@ fi
 
 source $HOME/$VIRTUALENV_NAME/bin/activate
 
-python -m visdom.server > visdom.log 2>&1 &
+python -m visdom.server $VISDOM_OPTIONS > visdom.log 2>&1 &
 
-python train.py --dataroot $DATAROOT --name geo_pix2pix_wgan_multiple_critic --model pix2pix_geo --which_model_netG unet_256 --which_direction BtoA --input_nc 3 --output_nc 3 --lambda_A 100 --lambda_B 100 --dataset_mode geo --no_lsgan --norm batch --pool_size 0 --no_html --div_threshold 1000 $OPTIONS --batchSize 4
+python train.py --dataroot $DATAROOT --name geo_pix2pix_wgan_multiple_critic --model pix2pix_geo --which_model_netG unet_256 --which_direction BtoA --input_nc 3 --output_nc 3 --lambda_A 100 --lambda_B 100 --dataset_mode geo --no_lsgan --norm batch --pool_size 0 --no_html --div_threshold 1000 $OPTIONS --batchSize 2
 
 kill %1
 
