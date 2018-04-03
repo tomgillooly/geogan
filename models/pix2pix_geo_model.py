3cb6293590154402d0a4729112178dfc1bb2f1ec
Tom Gillooly <thomas.gillooly@etu.univ-st-etienne.fr>
2018-04-03 16:07:06 +0200

    Random initialisation for Linear and BatchNorm layers as well

--- models/pix2pix_geo_model.py
+++ models/pix2pix_geo_model.py
@@ -27,6 +27,11 @@ def weights_init(m):
     elif classname.find('InstanceNorm') != -1:
         m.weight.data.normal_(1.0, 0.02)
         m.bias.data.fill_(0)
+    elif classname.find('Linear') != -1:
+        m.weight.data.normal_(0.0, 0.02)
+    elif classname.find('BatchNorm2d') != -1:
+        m.weight.data.normal_(1.0, 0.02)
+        m.bias.data.fill_(0.0)
 
 class DiscriminatorWGANGP(nn.Module):
 
