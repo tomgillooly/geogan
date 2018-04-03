0fe95891b4d18c553e47bc93647c3244522ec3a5
Tom Gillooly <thomas.gillooly@etu.univ-st-etienne.fr>
2018-04-03 16:10:02 +0200

    Increase batch size back to 4

--- pix2pix.sh
+++ pix2pix.sh
@@ -26,7 +26,7 @@ source $HOME/$VIRTUALENV_NAME/bin/activate
 python -m visdom.server $VISDOM_OPTIONS > visdom.log 2>&1 &
 
 python train.py --dataroot $DATAROOT --name geo_pix2pix_wgan_multiple_critic --model pix2pix_geo --which_model_netG unet_256 --which_direction BtoA --num_discrims 1 \
-	--input_nc 3 --output_nc 3 --lambda_A 100 --lambda_B 100 --dataset_mode geo --no_lsgan --norm batch --pool_size 0 --no_html --div_threshold 1000 $OPTIONS --batchSize 2
+	--input_nc 3 --output_nc 3 --lambda_A 100 --lambda_B 100 --dataset_mode geo --no_lsgan --norm batch --pool_size 0 --no_html --div_threshold 1000 $OPTIONS --batchSize 4
 
 kill %1
 
