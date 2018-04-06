e058bdfab413ed438be9232e2d30e7ae932b8201
Tom Gillooly <thomas.gillooly@etu.univ-st-etienne.fr>
2018-04-06 14:22:35 +0200

    Explicitly control concatenation dimension of weights

--- models/pix2pix_geo_model.py
+++ models/pix2pix_geo_model.py
@@ -471,7 +471,8 @@ class Pix2PixGeoModel(BaseModel):
         plate_weight = 1.0 - torch.sum(torch.sum(self.real_B_discrete_ROI[:, 1, :, :], dim=1), dim=1) / total_pixels
         subduction_weight = 1.0 - torch.sum(torch.sum(self.real_B_discrete_ROI[:, 2, :, :], dim=1), dim=1) / total_pixels
         
-        ce_fun = self.criterionCE(weight=torch.cat((ridge_weight, plate_weight, subduction_weight)))
+        weights = torch.cat((ridge_weight, plate_weight, subduction_weight), dim=1)
+        ce_fun = self.criterionCE(weight=weights)
 
         # print(fake_B_discrete_masked)
         # print(real_B_classes_masked)
