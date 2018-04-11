bf28d64440846ebba9f8f9e93fc715433516d98e
Tom Gillooly <thomas.gillooly@etu.univ-st-etienne.fr>
2018-04-11 16:44:24 +0200

    Set parameters to only remove a single class from the inpainted region

--- pix2pix.sh
+++ pix2pix.sh
@@ -26,9 +26,9 @@ source $HOME/$VIRTUALENV_NAME/bin/activate
 python -m visdom.server $VISDOM_OPTIONS > visdom.log 2>&1 &
 
 	# --continue_train --which_epoch latest --epoch_count 45 \
-python train.py --dataroot $DATAROOT --name geo_pix2pix_wgan_discrete_only \
+python train.py --dataroot $DATAROOT --name geo_pix2pix_wgan_inpaint_single_class \
 	--model pix2pix_geo --which_model_netG unet_256 --which_direction BtoA \
-	--high_iter 25 --low_iter 5 --discrete_only \
+	--high_iter 25 --low_iter 5 --inpaint_single_class \
 	--num_discrims 1 --which_model_netD wgan-gp --input_nc 3 --output_nc 3 \
 	--lambda_A 100 --lambda_B 100 --dataset_mode geo --no_lsgan --norm batch \
 	--pool_size 0 --no_html --div_threshold 1000 --batchSize 4 $OPTIONS
