5f267a4fbed95c16917bf55fb2d0d1bbab4cf6f5
Tom Gillooly <thomas.gillooly@etu.univ-st-etienne.fr>
2018-04-12 14:22:27 +0200

    Add parameters for non-local network

--- pix2pix.sh
+++ pix2pix.sh
@@ -28,9 +28,9 @@ source $HOME/$VIRTUALENV_NAME/bin/activate
 python -m visdom.server $VISDOM_OPTIONS > visdom.log 2>&1 &
 
 	# --continue_train --which_epoch latest --epoch_count 45 \
-python train.py --dataroot $DATAROOT --name geo_pix2pix_wgan_no_mask_to_critic \
-	--model pix2pix_geo --which_model_netG unet_256 --which_direction BtoA \
-	--high_iter 25 --low_iter 5 --no_mask_to_critic \
+python train.py --dataroot $DATAROOT --name geo_pix2pix_wgan_non_local \
+	--model pix2pix_geo --which_model_netG unet_256_non_local --which_direction BtoA \
+	--high_iter 25 --low_iter 5 \
 	--num_discrims 1 --which_model_netD wgan-gp --input_nc 3 --output_nc 3 \
 	--lambda_A 100 --lambda_B 100 --dataset_mode geo --no_lsgan --norm batch \
 	--pool_size 0 --no_html --div_threshold 1000 --batchSize 4 $OPTIONS
