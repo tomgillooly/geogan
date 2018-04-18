0a7b206f60bdd300bc98e2b8682fd854cd81c959
Tom Gillooly <thomas.gillooly@etu.univ-st-etienne.fr>
2018-04-18 15:18:07 +0200

    Add Hausdorff metric based on exclusive pairs

--- tests/test_hausdorff.py
+++ tests/test_hausdorff.py
@@ -3,7 +3,7 @@ import numpy as np
 import pytest
 import skimage.io as io
 
-from metrics.hausdorff import get_hausdorff
+from metrics.hausdorff import get_hausdorff, get_hausdorff_exc, get_pairs
 
 @pytest.fixture
 def images():
@@ -40,6 +40,45 @@ def test_two_to_one_point(images):
 	assert(d1 == 15)
 	assert(d2 == 10)
 
+
+def test_exclusive_pairs():
+	p1 = [(0, 0), (10, 10)]
+	p2 = [(1, 1), (2, 2)]
+
+	pairs = get_pairs(p1, p2)
+
+	assert(pairs == [(0, 0), (1, 1)])
+
+	p1 = [(10, 10), (0, 0)]
+	p2 = [(1, 1), (2, 2)]
+
+	pairs = get_pairs(p1, p2)
+
+	# Closest match always comes first
+	assert(pairs == [(1, 0), (0, 1)])
+
+
+def test_not_ordered():
+	p1 = [(0, 0), (10, 0)]
+	p2 = [(-10, 0), (9, 0)]
+
+	pairs = get_pairs(p1, p2)
+
+	assert(pairs == [(1, 1), (0, 0)])
+
+
+def test_unmatched_point():
+	p1 = [(0, 0), (10, 10)]
+	p2 = [(1, 1)]
+
+	pairs = get_pairs(p1, p2)
+
+	assert(pairs == [(0, 0), (1, None)])
+
+	pairs = get_pairs(p2, p1)
+
+	assert(pairs == [(0, 0)])
+
 # def test_problem_image():
 # 	mask_w = 386
 # 	mask_h = 75
@@ -47,7 +86,7 @@ def test_two_to_one_point(images):
 # 	im_gt = io.imread("results/geo_pix2pix_wgan_no_mask_to_critic/test_latest/images/serie100004_project_DIV_out_ground_truth_one_hot.png")[mask_h:mask_h+99, mask_w:mask_w+99, :]
 # 	im_p = io.imread("results/geo_pix2pix_wgan_no_mask_to_critic/test_latest/images/serie100004_project_DIV_out_output_one_hot.png")[mask_h:mask_h+99, mask_w:mask_w+99, :]
 
-# 	d1, d2, d3, im1, im2 = get_hausdorff(im_p[:, :, 0], im_gt[:, :, 0], visualise=True)
+# 	d1, d2, d3, im1, im2 = get_hausdorff_exc(im_p[:, :, 0], im_gt[:, :, 0], visualise=True)
 	
 # 	fig, ax = plt.subplots(1, 2)
 # 	ax = ax.ravel()
@@ -58,4 +97,4 @@ def test_two_to_one_point(images):
 # 	# ax[3].imshow(im2)
 # 	io.show()
 
-# 	assert(d1 == d2)
+# 	assert(d1 != d2)
