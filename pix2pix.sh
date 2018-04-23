d036a82e9ec3fad456462e4e22da4808d41b4511
Tom Gillooly <thomas.gillooly@etu.univ-st-etienne.fr>
2018-04-23 15:27:10 +0200

    Loss function only in local region

--- pix2pix.sh
+++ pix2pix.sh
@@ -30,7 +30,7 @@ python -m visdom.server $VISDOM_OPTIONS > visdom.log 2>&1 &
 	# --continue_train --which_epoch latest --epoch_count 45 \
 python train.py --dataroot $DATAROOT --name base_autoencoder \
 	--model pix2pix_geo --which_model_netG unet_256 --which_direction BtoA \
-	--high_iter 25 --low_iter 5 \
+	--high_iter 25 --low_iter 5 --local_loss \
 	--num_discrims 0 --which_model_netD wgan-gp --input_nc 3 --output_nc 3 \
 	--lambda_A 100 --lambda_B 100 --dataset_mode geo --no_lsgan --norm batch \
 	--pool_size 0 --no_html --div_threshold 1000 --batchSize 4 $OPTIONS
