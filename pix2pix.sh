fbeb7427944762f28bfa361779cc20be92016c21
Tom Gillooly <thomas.gillooly@etu.univ-st-etienne.fr>
2018-05-14 13:03:29 +0200

    Make sure we're loading data from the records

--- pix2pix.sh
+++ pix2pix.sh
@@ -9,7 +9,8 @@ if [ "$HOSTNAME" == "tomoplata-OptiPlex-790" ]; then
 	OPTIONS="--gpu_ids -1 --display_id 0"
 else
 	VIRTUALENV_NAME=cyclegan3
-	DATAROOT=/storage/Datasets/Geology-NicolasColtice/DS2-1810-RAW-DAT
+	# DATAROOT=/storage/Datasets/Geology-NicolasColtice/DS2-1810-RAW-DAT
+	DATAROOT=/storage/Datasets/Geology-NicolasColtice/pytorch_records
 	HOME=/home/tgillooly/
 
 	source find_free_port.sh
@@ -28,7 +29,7 @@ source $HOME/$VIRTUALENV_NAME/bin/activate
 python -m visdom.server $VISDOM_OPTIONS > visdom.log 2>&1 &
 
 	# --continue_train --which_epoch latest --epoch_count 45 \
-python train.py --dataroot $DATAROOT --name autoencoder_base_bigger_batch \
+python train.py --dataroot $DATAROOT --name autoencoder_base \
 	--model pix2pix_geo --which_direction BtoA \
 	--dataset_mode geo --no_lsgan --norm batch \
 	--input_nc 3 --output_nc 3 \
