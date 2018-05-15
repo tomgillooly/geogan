ccf6dfd3d4cca6dc864fb9e556b41a3902b055be
Tom Gillooly <thomas.gillooly@etu.univ-st-etienne.fr>
2018-05-15 15:16:25 +0200

    Turn off folder prediction and turn on continent data

--- pix2pix.sh
+++ pix2pix.sh
@@ -29,13 +29,13 @@ source $HOME/$VIRTUALENV_NAME/bin/activate
 python -m visdom.server $VISDOM_OPTIONS > visdom.log 2>&1 &
 
 	# --continue_train --which_epoch latest --epoch_count 45 \
-python train.py --dataroot $DATAROOT --name autoencoder_weighted_folder_pred \
+python train.py --dataroot $DATAROOT --name autoencoder_continent_data \
 	--model pix2pix_geo --which_direction BtoA \
 	--dataset_mode geo --no_lsgan --norm batch \
 	--input_nc 3 --output_nc 3 \
-	--lambda_A 100 --lambda_B 100 --lambda_D 100 \
-	--folder_pred \
+	--lambda_A 100 --lambda_B 100 \
 	--num_discrims 0 \
+	--continent_data \
 	--which_model_netG unet_256 \
 	--discrete_only \
 	--pool_size 0 --no_html --div_threshold 1000 --batchSize 4 $OPTIONS
