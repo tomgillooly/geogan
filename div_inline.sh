344e9fc2d54b85ee1adf829fd068b1d0a79ca401
Tom Gillooly <thomas.gillooly@etu.univ-st-etienne.fr>
2018-05-18 13:58:30 +0200

    Set parameters to train DIV inline with local loss

--- div_inline.sh
+++ div_inline.sh
@@ -29,12 +29,13 @@ source $HOME/$VIRTUALENV_NAME/bin/activate
 python -m visdom.server $VISDOM_OPTIONS > visdom.log 2>&1 &
 
 	# --continue_train --which_epoch 100 --epoch_count 101 --niter 300 --niter_decay 100 \
-python train.py --dataroot $DATAROOT --name div_inline_ae_base \
+python train.py --dataroot $DATAROOT --name div_inline_ae_local_loss \
 	--model div_inline --which_direction BtoA \
 	--no_lsgan --norm batch \
 	--input_nc 3 --output_nc 1 \
 	--lambda_D 100 \
 	--which_model_netG unet_256 \
+	--local_loss \
 	--pool_size 0 --no_html --batchSize 4 $OPTIONS
 
 kill %1
