7960bbafedfd2cb39098537aabd71911c8c4c60b
Tom Gillooly <thomas.gillooly@etu.univ-st-etienne.fr>
2018-05-22 09:46:58 +0200

    Add trailing backslash to run script

--- pix2pix.sh
+++ pix2pix.sh
@@ -35,7 +35,7 @@ python train.py --dataroot $DATAROOT --name wgan_long_iter_base \
 	--input_nc 3 --output_nc 3 \
 	--high_iter 500 --low_iter 50 \
 	--lambda_A 0.01 --lambda_B 100 --lambda_D 100 \
-	--discrete_only
+	--discrete_only \
 	--num_discrims 1 \
 	--which_model_netG unet_256 \
 	--pool_size 0 --no_html --div_threshold 1000 --batchSize 4 $OPTIONS
