27207d787219bb414f095ae7c843af329a17e54b
Tom Gillooly <tomgillooly@gmail.com>
2018-05-30 22:39:37 +0200

    Change min/max, threshold and histogram data types

--- data/geo_unpickler.py
+++ data/geo_unpickler.py
@@ -138,7 +138,13 @@ class GeoUnpickler(object):
 
 			data_dict[key] = item
 
-		for key in ['DIV_max', 'DIV_min', 'DIV_thresh', 'conn_comp_hist']:
+		for key in ['DIV_max', 'DIV_min']:
+			if not key in data_dict.keys():
+				continue
+
+			data_dict[key] = torch.DoubleTensor(data_dict[key])
+
+		for key in ['DIV_thresh', 'conn_comp_hist']:
 			if not key in data_dict.keys():
 				continue
 
