037a7135e08c80da6d1b34ab5f11d5afc4cb62ed
Tom Gillooly <thomas.gillooly@etu.univ-st-etienne.fr>
2018-06-13 14:46:28 +0200

    Set parameters to continue training now that data is fixed

--- circle.sh
+++ circle.sh
@@ -29,10 +29,10 @@ source $HOME/$VIRTUALENV_NAME/bin/activate
 
 python -m visdom.server $VISDOM_OPTIONS > visdom.log 2>&1 &
 
-	# --continue_train --which_epoch 100 --epoch_count 101 --niter 300 --niter_decay 100 \
 	# --high_iter 25 --low_iter 5 \
 python train.py --dataroot $DATAROOT --name circle_div_non_filled_global_batch_1000_no_mask_bias \
 	--model div_inline --which_direction BtoA \
+	--continue_train --which_epoch 1 --epoch_count 1 \
 	--num_discrims 0 --alpha 0 \
 	--no_lsgan --norm batch \
 	--input_nc 3 --output_nc 1 \
