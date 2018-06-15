085b27b55f4e29aef608c4ebc7f1289ed34e75c3
Tom Gillooly <thomas.gillooly@etu.univ-st-etienne.fr>
2018-06-15 12:12:50 +0200

    Make sure all DIV images are converted to discrete

--- models/div_inline.py
+++ models/div_inline.py
@@ -280,14 +280,19 @@ class DivInlineModel(BaseModel):
 
         self.fake_B_DIV = self.netG(self.G_input)
         
-        A_DIV = self.fake_B_DIV.cpu().numpy().squeeze()
-        A_DIV = np.interp(A_DIV, [np.min(A_DIV), 0, np.max(A_DIV)], [self.div_min, 0, self.div_max])
+        self.fake_B_discrete = torch.zeros(self.real_B_discrete.shape)
 
-        tmp_dict = {'A_DIV': A_DIV}
-        self.p.create_one_hot(tmp_dict, self.div_thresh, skel=False)
-        self.fake_B_discrete = tmp_dict['A']
+        for i in range(self.fake_B_discrete.shape[0]):
+            A_DIV = self.fake_B_DIV[i].cpu().numpy().squeeze()
+            A_DIV = np.interp(A_DIV, [np.min(A_DIV), 0, np.max(A_DIV)], [self.div_min, 0, self.div_max])
 
-        self.fake_B_DIV = self.fake_B_DIV.cuda() if len(self.gpu_ids) > 0 else self.fake_B_DIV
+            tmp_dict = {'A_DIV': A_DIV}
+            self.p.create_one_hot(tmp_dict, self.div_thresh, skel=False)
+            self.fake_B_discrete[i] = tmp_dict['A']
+
+            self.fake_B_DIV = self.fake_B_DIV.cuda() if len(self.gpu_ids) > 0 else self.fake_B_DIV
+            
+        self.fake_B_discrete = self.fake_B_discrete.cuda() if len(self.gpu_ids) > 0 else self.fake_B_discrete
 
         if self.opt.isTrain and self.opt.num_folders > 1 and self.opt.folder_pred:
             self.fake_folder = self.folder_fc(self.netG.inner_layer.output.view(self.batch_size, -1))
