de1eec33b6a30e5f7457f9333ea8225b55ce2a50
Tom Gillooly <thomas.gillooly@etu.univ-st-etienne.fr>
2018-06-15 12:03:37 +0200

    See if we can get away with just cloning DIV tensor, rather than re-CUDA-ing

--- models/div_inline.py
+++ models/div_inline.py
@@ -279,10 +279,10 @@ class DivInlineModel(BaseModel):
 
 
         self.fake_B_DIV = self.netG(self.G_input)
-        self.fake_B_DIV = self.fake_B_DIV.cuda() if len(self.gpu_ids) > 0 else self.fake_B_DIV
+        # self.fake_B_DIV = self.fake_B_DIV.cuda() if len(self.gpu_ids) > 0 else self.fake_B_DIV
 
         
-        A_DIV = self.fake_B_DIV.data.numpy().squeeze()
+        A_DIV = self.fake_B_DIV.clone().numpy().squeeze()
         A_DIV = np.interp(A_DIV, [np.min(A_DIV), 0, np.max(A_DIV)], [self.div_min, 0, self.div_max])
 
         tmp_dict = {'A_DIV': A_DIV}
