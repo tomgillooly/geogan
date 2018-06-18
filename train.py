00240c9873e08a5be39238de75da1e1e688bf2f0
Tom Gillooly <tomgillooly@gmail.com>
2018-06-18 21:20:50 +0200

    Take running error and average when plotting/printing

--- train.py
+++ train.py
@@ -4,6 +4,8 @@ from data.geo_unpickler import GeoUnpickler
 from models.models import create_model
 from util.visualizer import Visualizer
 
+from collections import defaultdict
+
 import glob
 import sys
 
@@ -32,6 +34,8 @@ def train():
     visualizer = Visualizer(opt)
     total_steps = 0
 
+    running_errors = defaultdict(list)
+
     for epoch in range(opt.epoch_count, opt.niter + opt.niter_decay + 1):
         epoch_start_time = time.time()
         epoch_iter = 0
@@ -44,16 +48,24 @@ def train():
             model.set_input(data)
             model.optimize_parameters(step_no=total_steps)
 
+            for key, item in model.get_current_errors():
+                running_errors[key].append(item)
+
             if total_steps % opt.display_freq == 0:
                 save_result = total_steps % opt.update_html_freq == 0
                 visualizer.display_current_results(model.get_current_visuals(), epoch, save_result)
 
             if total_steps % opt.print_freq == 0:
-                errors = model.get_current_errors()
+                average_errors = {}
+                for key, error_list in running_errors:
+                    average_errors[key] = sum(error_list) / len(error_list)
+                
+                running_errors = defaultdict(list)
+
                 t = (time.time() - iter_start_time) / opt.batchSize
-                visualizer.print_current_errors(epoch, epoch_iter, errors, t)
+                visualizer.print_current_errors(epoch, epoch_iter, average_errors, t)
                 if opt.display_id > 0:
-                    visualizer.plot_current_errors(epoch, float(epoch_iter) / (dataset_size * opt.batchSize), opt, errors)
+                    visualizer.plot_current_errors(epoch, float(epoch_iter) / (dataset_size * opt.batchSize), opt, average_errors)
 
             if total_steps % opt.save_latest_freq == 0:
                 print('saving the latest model (epoch %d, total_steps %d)' %
