55de00e0c5b4e3b7b581347bbd01f2ee4322215e
Tom Gillooly <tomgillooly@gmail.com>
2018-06-24 12:19:20 +0200

    Set parameters to transfer circle model weights and continue training

--- circle.sh
+++ circle.sh
@@ -30,15 +30,14 @@ source $HOME/$VIRTUALENV_NAME/bin/activate
 python -m visdom.server $VISDOM_OPTIONS > visdom.log 2>&1 &
 
 	# --high_iter 25 --low_iter 5 \
-	# --continue_train --which_epoch latest --epoch_count 4 \
-python train.py --dataroot $DATAROOT --name ellipse_rmsprop \
+python train.py --dataroot $DATAROOT --name ellipse_circle_restart \
 	--model div_inline --which_direction BtoA \
-	--num_discrims 0 --alpha 0 \
+	--continue_train --which_epoch 55 --epoch_count 56 \
+	--num_discrims 0 \
 	--no_lsgan --norm batch \
 	--diff_in_numerator \
 	--local_loss \
 	--grad_loss \
-	--optim_type rmsprop --lr 0.001 --alpha 0.9 \
 	--input_nc 3 --output_nc 1 \
 	--lambda_A 20 --lambda_D 100 \
 	--which_model_netG unet_256 \
