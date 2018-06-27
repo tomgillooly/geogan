1ac7886eba87b6b748e75b7c89984c656f13ba32
Tom Gillooly <tomgillooly@gmail.com>
2018-06-27 18:42:44 +0200

    Change plot loss to run on cluster

--- prep_circle_results.sh
+++ prep_circle_results.sh
@@ -8,30 +8,29 @@
 # 		mkdir checkpoints/$model
 # 	fi
 
-# 	for file in `ssh oggy "egrep -l $model" geo_gan.git/slurm*`
-# 	do
-# 		scp oggy:$file checkpoints/$model/
-# 	done
-# 	scp oggy:geo_gan.git/checkpoints/$model/latest* checkpoints/$model/
+	# for file in `ssh oggy "egrep -l $model" geo_gan.git/slurm*`
+	# do
+	# 	scp oggy:$file checkpoints/$model/
+	# done
+	# scp oggy:geo_gan.git/checkpoints/$model/latest* checkpoints/$model/
 
-# 	scp oggy:geo_gan.git/checkpoints/$model/opt.txt checkpoints/$model/
+	# scp oggy:geo_gan.git/checkpoints/$model/opt.txt checkpoints/$model/
 
-# 	scp oggy:geo_gan.git/checkpoints/$model/loss_log.txt checkpoints/$model/
+	# scp oggy:geo_gan.git/checkpoints/$model/loss_log.txt checkpoints/$model/
 
-# 	if [ ! -e checkpoints/$model/latest_net_G.pth ]; then
-# 		exit 1
-# 	fi
+	# if [ ! -e checkpoints/$model/latest_net_G.pth ]; then
+		# exit 1
+	# fi
 # done
-if [ $HOSTNAME == 'liris501-r073' ]; then
-	for model in `cat models_to_test`
-	do
-		# echo $model
 
-		egrep -l $model slurm* | xargs -n1 -I{} cp {} checkpoints/$model/
+for model in `cat models_to_test`
+do
+	# echo $model
 
-		cp checkpoints/$model/opt.txt{,.train}
-	done
-fi
+	egrep -l $model slurm* | xargs -n1 -I{} cp {} checkpoints/$model/
+
+	cp checkpoints/$model/opt.txt{,.train}
+done
 
 for model in `cat models_to_test`
 do
@@ -42,7 +41,7 @@ do
 		bash test_circle.sh $model
 	fi
 
-	python assorted_test/plot_loss.py $model
+	sbatch "#!/bin/bash; source ~/cyclegan3/bin/activate; python assorted_test/plot_loss.py $model; deactivate"
 done
 
 # for model in `cat models_to_test`
