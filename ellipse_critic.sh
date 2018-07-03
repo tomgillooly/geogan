e12af934dac57f767e8ebc0319b9cfe564fc5c0d
Tom Gillooly <thomas.gillooly@etu.univ-st-etienne.fr>
2018-07-03 10:49:37 +0200

    Update patch gan script with separate lr rates

--- ellipse_critic.sh
+++ ellipse_critic.sh
@@ -9,7 +9,7 @@ else
 	VIRTUALENV_NAME=cyclegan3
 	# DATAROOT=/storage/Datasets/Geology-NicolasColtice/DS2-1810-RAW-DAT
 	# DATAROOT=/storage/Datasets/Geology-NicolasColtice/pytorch_records_new_thresh
-	DATAROOT=/storage/Datasets/Geology-NicolasColtice/ellipses3_overfit
+	DATAROOT=/storage/Datasets/Geology-NicolasColtice/ellipses3
 	HOME=/home/tgillooly/
 
 	source find_free_port.sh
@@ -29,16 +29,16 @@ python -m visdom.server $VISDOM_OPTIONS > visdom.log 2>&1 &
 
 	# --high_iter 25 --low_iter 5 \
 	# --continue_train --which_epoch 55 --epoch_count 56 \
-python train.py --dataroot $DATAROOT --name ellipse_critic_patchgan_ttur \
+python train.py --dataroot $DATAROOT --name ellipse_critic_patchgan_orthogonal \
 	--model div_inline --which_direction BtoA \
-	--num_discrims 1 --which_model_netD n_layers \
-	--no_lsgan --norm batch \
+	--num_discrims 1 --which_model_netD n_layers --n_layers_D 4 \
+	--no_lsgan --norm batch --init_type orthogonal \
 	--high_iter 1 --low_iter 1 \
 	--diff_in_numerator \
 	--local_loss \
 	--input_nc 3 --output_nc 1 \
         --lr 0.0004 \
-	--lambda_A 0.1 --lambda_D 100 \
+	--lambda_A 0.01 --lambda_D 100 \
 	--which_model_netG unet_256 \
 	--display_freq 10 --print_freq 10 \
 	--pool_size 0 --no_html --batchSize 5 --nThreads 2 $OPTIONS
--- patchgan_orthogonal.sh
+++ patchgan_orthogonal.sh
@@ -37,7 +37,7 @@ python train.py --dataroot $DATAROOT --name ellipse_critic_patchgan_orthogonal \
 	--diff_in_numerator \
 	--local_loss \
 	--input_nc 3 --output_nc 1 \
-        --lr 0.0004 \
+        --g_lr 0.0004 --d_lr 0.0001 \
 	--lambda_A 0.01 --lambda_D 100 \
 	--which_model_netG unet_256 \
 	--display_freq 10 --print_freq 10 \
