411092dab7d58d3d198de7d8ab935bafe5428e42
Tom Gillooly <thomas.gillooly@etu.univ-st-etienne.fr>
2018-08-28 17:22:56 +0200

    Set parameters to train on mixed continent data

--- geo_self_attn_cont.sh
+++ geo_self_attn_cont.sh
@@ -9,7 +9,7 @@ else
 	VIRTUALENV_NAME=cyclegan3
 	# DATAROOT=/storage/Datasets/Geology-NicolasColtice/DS2-1810-RAW-DAT
 	# DATAROOT=/storage/Datasets/Geology-NicolasColtice/pytorch_records_new_thresh
-	DATAROOT=/storage/Datasets/Geology-NicolasColtice/old_pytorch_records_cont
+	DATAROOT=/storage/Datasets/Geology-NicolasColtice/pytorch_records_cont
 	HOME=/home/tgillooly/
 
 	source find_free_port.sh
@@ -27,9 +27,9 @@ source $HOME/$VIRTUALENV_NAME/bin/activate
 
 python -m visdom.server $VISDOM_OPTIONS > visdom.log 2>&1 &
 
+	#--continue_train --which_epoch 1900 --epoch_count 1901 \
 	# --high_iter 25 --low_iter 5 \
-	# --continue_train --which_epoch 55 --epoch_count 56 \
-python train.py --dataroot $DATAROOT --name geo_self_attn_old_continents --x_size 512 \
+python train.py --dataroot $DATAROOT --name geo_self_attn_continents --x_size 512 \
         --model div_inline --which_direction BtoA \
         --niter 8000 --niter_decay 2000 \
 	--num_discrims 1 --which_model_netD self-attn --use_hinge \
@@ -38,12 +38,12 @@ python train.py --dataroot $DATAROOT --name geo_self_attn_old_continents --x_siz
 	--local_loss \
         --continent_data \
 	--input_nc 3 --output_nc 1 \
-        --g_lr 0.0004 --d_lr 0.0001 \
+        --g_lr 0.0001 --d_lr 0.0004 \
 	--lambda_A 0.1 --lambda_B 0.005 --lambda_D 100 \
 	--which_model_netG unet_256 \
 	--display_freq 10 --print_freq 10 --save_epoch_freq 50 \
 	--pool_size 0 --no_html --batchSize 5 --nThreads 2 $OPTIONS
 
-#kill %1
+kill %1
 
-#deactivate
+deactivate
